4e76918223b825ff020eba1e0589236f387e738d
DRV111 <xblackcrownx@gmail.com>
2024-01-28 14:03:32 +0300

    feat: convert money with API

--- src/store.js
+++ src/store.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import accountReducer from './features/accounts/accountSlice';
 import customerReducer from './features/customers/customerSlice';
+import { thunk } from 'redux-thunk';
 
 const rootReducers = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducers);
+const store = createStore(rootReducers, applyMiddleware(thunk));
 
 export default store;

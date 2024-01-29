02fb17d23587498a275df8ecdf849440092df06a
DRV111 <xblackcrownx@gmail.com>
2024-01-29 12:23:03 +0300

    refactor: refactor accountSlice by usin redux-toolkit

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,11 +1,46 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: '',
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: 'account',
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan(state, action) {
+      if (state.loan > 0) return;
+
+      state.loan = action.payload.amount;
+      state.loanPurpose = action.payload.purpose;
+      state.balance = state.balance + action.payload.amount;
+    },
+    payLoan(state, action) {
+      state.loan = 0;
+      state.loanPurpose = '';
+      state.balance -= state.loan;
+    },
+  },
+});
+
+console.log(accountSlice);
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
+
+/*
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case 'account/deposit':
       return {
@@ -71,3 +106,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: 'account/payLoan' };
 }
+*/
